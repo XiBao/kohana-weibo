4b3eb1ab779b28897d838fb99b0c6978c36f3280
JeremyWei <shuimuqingshu@gmail.com>
2012-06-21 21:22:36 +0800

    using single instance

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-kohana-weibo
-============
+#kohana-weibo
+
+## Installation
+git submodule git@github.com:JeremyWei/kohana-weibo.git modules/weibo
+git submodule update --recursive
+
+## Using Weibo
+This module is a wrapper of weibo PHP SDK, and can supply full of the features,
+just use it like original SDK.
+这个module对微博的PHP SDK进行了包装，能提供所有的原始特性，就像使用原始的SDK那样来使用就可以了。
+
+## Example
+// Generate authorization url
+$oauth = Weibo::factory('oauth');
+$callback = 'http://example.com/callback';
+$url = $oauth->getAuthorizeURL($callback);
+
+// Authorization
+$post = $this->request->post();
+if (isset($post['code'])) {
+	$keys = array();
+	$keys['code'] = $post['code'];
+	$keys['redirect_uri'] = $callback;
+	try {
+		$token = $oauth->getAccessToken('code', $keys);
+	} catch (OAuthException $e) {
+	}
+}
+
+// Call method
+$client = Weibo::factory('client', $access_token);
+$user_info = $client->show_user_by_id($uid); // invoke method defined in class SaeTClientV2
+
--- classes/kohana/weibo.php
+++ classes/kohana/weibo.php
@@ -7,108 +7,64 @@
  * @category   SDK
  * @author     Jeremy Wei
  * @copyright  (c) 2012 Jeremy Wei
- * @license    http://kohanaphp.com/license.html
+ * @license    http://www.opensource.org/licenses/mit-license.php
  */
 class Kohana_Weibo {
 
 	/**
-	 * OAuth 
+	 * SaeTOAuthV2 instance
 	 *
 	 * @var SaeTOAuthV2
 	 **/
-	protected $sae_oauth;
+	static public $oauth;
 
 	/**
-	 * OAuth client
+	 * SaeTClientV2 instance 
 	 *
 	 * @var SaeTClientV2
 	 **/
-	protected $sae_client;
+	static public $client;
 
 	/**
-	 * App key
-	 *
-	 * @var string
+	 * Factory
+	 * 
+	 * @param $type string ['oauth' or 'client'] 
+	 * @return mixed
 	 **/
-	protected $app_key;
-
-	/**
-	 * App secret
-	 *
-	 * @var string
-	 **/
-	protected $app_secret;
-
-	/**
-	 * Access token
-	 *
-	 * @var string
-	 **/
-	protected $access_token;
-
-	/**
-	 * return instance of SaeTOAuthV2
-	 *
-	 * @return SaeTOAuthV2
-	 **/
-	public function get_oauth()
-	{
-		// Create SaeTOAuthV2 instance
-		$this->sae_oauth = new SaeTOAuthV2($this->app_key, $this->app_secret);
-
-		return $this->sae_oauth;
-	}
-
-	/**
-	 * return instance of SaeTClientV2
-	 *
-	 * @return SaeTClientV2
-	 **/
-	public function get_client($access_token)
-	{
-		if (!$access_token)
-		{
-			throw new Exception("access_token is missing");
-		}
-		else
-		{
-			$this->access_token = $access_token;
-		}
-
-		//
-		$this->sae_client = new SaeTClientV2($this->app_key, $this->app_secret, $this->access_token);
-
-		return $this->sae_client;
-	}
-
-	/**
-	 * Constructor
-	 *
-	 * @return void
-	 **/
-	public function __construct($app_key=NULL, $app_secret=NULL)
+	public static function factory($type='oauth', $access_token='')
 	{
 		// load class from vendor
-		require Kohana::find_file('vendor', 'saetv2.ex.class');
+		if (!class_exists('SaeTOAuthV2') || !class_exists('SaeTClientV2'))
+		{
+			require Kohana::find_file('vendor', 'saetv2.ex.class');	
+		}
+		
+		switch ($type) {
 
-		// If app_key empty then load it from kohana config
-		if (!$app_key)
-		{
-			$this->app_key = Kohana::$config->load('weibo.app_key');
-		}
-		else
-		{
-			$this->app_key = $app_key;
-		}
+			case 'oauth':
 
-		// If app_secret empty then load it from kohana config
-		if (!$app_secret)
-		{
-			$this->app_secret = Kohana::$config->load('weibo.app_secret');
-		}
-		else
-		{
-			$this->app_secret = $app_secret;
+				if (!self::$oauth)
+				{
+					self::$oauth = new SaeTOAuthV2(Kohana::$config->load('weibo.app_key'), 
+						Kohana::$config->load('weibo.app_secret'));
+				}
+
+				return self::$oauth;
+				break;
+			
+			case 'client':
+
+				if (!self::$client)
+				{
+					self::$client = new SaeTClientV2(Kohana::$config->load('weibo.app_key'), 
+						Kohana::$config->load('weibo.app_secret'), $access_token);
+				}
+
+				return self::$client;
+				break;
+				
+			default:
+				break;
 		}
 	}
 }
